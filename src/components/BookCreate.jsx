9c0186e05e5bd48e55213f3a792d74b3e0b02f8c
Noi-Git <noi.patsin@gmail.com>
2023-03-13 19:37:30 -0400

    initial commit

---
+++ src/components/BookCreate.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+const BookCreate = () => {
+  return <div>BookCreate</div>
+}
+
+export default BookCreate
---
+++ src/components/BookEdid.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BookEdid = () => {
+  return <div>BookEdid</div>
+}
+
+export default BookEdid
