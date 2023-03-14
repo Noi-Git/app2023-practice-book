1e025d986b4bb22c719e73e9b687467912bbb75d
Noi-Git <noi.patsin@gmail.com>
2023-03-14 00:22:59 -0400

    add delete and edit function

--- src/App.js
+++ src/App.js
@@ -15,9 +15,30 @@ function App() {
     setBooks(updatedBook)
   }
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      // if (book.id !== id) {
+      //   return book
+      // }
+      return book.id !== id
+    })
+    setBooks(updatedBooks)
+  }
+
+  const editBookById = (id, title) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title }
+      }
+      return book
+    })
+
+    setBooks(updatedBooks)
+  }
+
   return (
     <div className='app'>
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   )
--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-const BookShow = ({ book }) => {
-  return <div className='book-show'>{book.title}</div>
+const BookShow = ({ book, onDelete, onEdit }) => {
+  const handleClick = (id) => {
+    onDelete(book.id)
+  }
+
+  const handleEdit = (id, title) => {
+    console.log('you click me')
+    onEdit(id, title)
+  }
+
+  return (
+    <div className='book-show'>
+      <div className='actions'>
+        <button className='edit' onClick={handleEdit}>
+          Edit
+        </button>
+        <button className='delete' onClick={handleClick}>
+          Delete
+        </button>
+      </div>
+      {book.title}
+    </div>
+  )
 }
 
 export default BookShow
