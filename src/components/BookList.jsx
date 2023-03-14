30fb3a33c1b66e13673e97d423777728034835e9
Noi-Git <noi.patsin@gmail.com>
2023-03-13 23:48:20 -0400

    pass props from App to BookList then BookShow

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import BookShow from './BookShow'
 
-const BookList = () => {
-  return <div>BookList</div>
+const BookList = ({ books }) => {
+  const renderedBook = books.map((book) => {
+    return <BookShow key={book.id} book={book} />
+  })
+
+  return <div className='book-list'>{renderedBook}</div>
 }
 
 export default BookList
--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BookShow = () => {
+const BookShow = ({ book }) => {
   return <div>BookShow</div>
 }
 
