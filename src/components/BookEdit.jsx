f74d69f60d478f3ae77ded9acb5a7e89eb59181f
Noi-Git <noi.patsin@gmail.com>
2023-03-14 17:01:24 -0400

    crete book edit form

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BookEdit = () => {
-  return <div>BookEdit</div>
+const BookEdit = ({ onEdit }) => {
+  const [title, setTitle] = useState('')
+
+  const handleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Hey!, you clicked me. Here is you title:', title)
+  }
+
+  return (
+    <form className='book-edit' onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input
+        type='text'
+        className='input'
+        value={title}
+        onChange={handleChange}
+      />
+      <button className='button is-primary'>Save Chenge</button>
+    </form>
+  )
 }
 
 export default BookEdit
