006db6b72196a26e17f08c8f0fc22fb687eec674
Noi-Git <noi.patsin@gmail.com>
2023-03-14 22:46:31 -0400

    setup Provider and example of simple use case

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,7 @@ import BooksContext from '../context/books'
 import BookShow from './BookShow'
 
 const BookList = ({ books, onDelete, onEdit }) => {
-  const value = useContext(BooksContext)
+  const { count, incrementCount } = useContext(BooksContext)
 
   const renderedBook = books.map((book) => {
     return (
@@ -13,7 +13,10 @@ const BookList = ({ books, onDelete, onEdit }) => {
 
   return (
     <div className='book-list'>
-      {value}
+      {count}
+      <button className='button is-primary' onClick={incrementCount}>
+        increment count
+      </button>
       {renderedBook}
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BooksContext from './context/books'
+import { Provider } from './context/books'
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BooksContext.Provider value={5}>
+  <Provider>
     <App />
-  </BooksContext.Provider>
+  </Provider>
 )
