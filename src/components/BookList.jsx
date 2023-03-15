99245aebb622309e2b00bbe6cbaf65b3a6d9b171
Noi-Git <noi.patsin@gmail.com>
2023-03-14 23:11:25 -0400

    cleanup + remove functions from App.js to books.js

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,23 +3,13 @@ import BooksContext from '../context/books'
 import BookShow from './BookShow'
 
 const BookList = ({ books, onDelete, onEdit }) => {
-  const { count, incrementCount } = useContext(BooksContext)
-
   const renderedBook = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     )
   })
 
-  return (
-    <div className='book-list'>
-      {count}
-      <button className='button is-primary' onClick={incrementCount}>
-        increment count
-      </button>
-      {renderedBook}
-    </div>
-  )
+  return <div className='book-list'>{renderedBook}</div>
 }
 
 export default BookList
--- src/context/books.js
+++ src/context/books.js
@@ -3,20 +3,50 @@ import { createContext, useState } from 'react'
 const BooksContext = createContext()
 
 function Provider({ children }) {
-  const [count, setCount] = useState(5)
+  const [books, setBooks] = useState([])
 
-  const valueToShare = {
-    count,
-    incrementCount: () => {
-      setCount(count + 1)
-    },
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+
+    setBooks(response.data)
   }
 
-  return (
-    <BooksContext.Provider value={valueToShare}>
-      {children}
-    </BooksContext.Provider>
-  )
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title, //request body
+    })
+
+    const updatedBook = [...books, response.data]
+
+    setBooks(updatedBook)
+  }
+
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`)
+
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id
+    })
+    setBooks(updatedBooks)
+  }
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    })
+    // console.log(response)
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data }
+      }
+      return book
+    })
+
+    setBooks(updatedBooks)
+  }
+
+  return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>
 }
 
 export { Provider }
