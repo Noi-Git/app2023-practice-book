9421200422b5b383f14a77a6c84d0349e1e07d65
Noi-Git <noi.patsin@gmail.com>
2023-03-14 23:10:31 -0400

    cleanup + remove functions from App.js to books.js

--- src/App.js
+++ src/App.js
@@ -5,53 +5,9 @@ import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
 
 function App() {
-  const [books, setBooks] = useState([])
-
-  const fetchBooks = async () => {
-    const response = await axios.get('http://localhost:3001/books')
-
-    setBooks(response.data)
-  }
-
   useEffect(() => {
     fetchBooks()
   }, [])
-
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books', {
-      title, //request body
-    })
-
-    const updatedBook = [...books, response.data]
-
-    setBooks(updatedBook)
-  }
-
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`)
-
-    const updatedBooks = books.filter((book) => {
-      // if (book.id !== id) { return book }
-      return book.id !== id
-    })
-    setBooks(updatedBooks)
-  }
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    })
-    // console.log(response)
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data }
-      }
-      return book
-    })
-
-    setBooks(updatedBooks)
-  }
 
   return (
     <div className='app'>
