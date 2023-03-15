b44c077c3938638ff79fd5ac6dddfaae5868695e
Noi-Git <noi.patsin@gmail.com>
2023-03-14 22:23:46 -0400

    setup useContext and example of basic use case

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import BooksContext from '../context/books'
 import BookShow from './BookShow'
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const value = useContext(BooksContext)
+
   const renderedBook = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     )
   })
 
-  return <div className='book-list'>{renderedBook}</div>
+  return (
+    <div className='book-list'>
+      {value}
+      {renderedBook}
+    </div>
+  )
 }
 
 export default BookList
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import BooksContext from './context/books'
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <BooksContext.Provider value={5}>
     <App />
-  </React.StrictMode>
+  </BooksContext.Provider>
 )
